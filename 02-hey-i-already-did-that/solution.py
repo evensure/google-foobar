5da1a53e0e411db30f3fc6ea43067a6a00c0f9e5
Justus Evenschor <j.evenschor@gmail.com>
2022-10-18 18:25:18 +0200

    challenge 2.1 solved

--- 02-hey-i-already-did-that/solution.py
+++ 02-hey-i-already-did-that/solution.py
@@ -1,24 +1,34 @@
+import numpy as np
+
 def solution(n, b):
     '''
-    param n: minion ID of length k in base b
-    type n: str
-    param b: base of number n (constr: 2 <= b <= 10)
-    type b: int
+    n: minion ID of length k in base b
+    b: base of number n (constr: 2 <= b <= 10)
+    k_n: length of n (constr: 2 <= k <= 9)
     '''
-    # param k: length of n (constr: 2 <= k <= 9)
-    # type k: int
-    k = len(n)
+    k_n = len(n)
+    list_of_id = []
 
+    while n not in list_of_id:
+        list_of_id.append(n)
+        y = ''.join(sorted(n))
+        x = y[::-1]
+        '''
+        x: n descending
+        y: n ascending
+        '''
+        y = ''.join(sorted(n))
+        x = y[::-1]
+        # z_int: integer represantation of z for base b
+        z_int = int(x, b) - int(y, b)
+        # k_z: length of z without padding
+        k_z = len(np.base_repr(z_int, b))
+        # z: string represantation of z_int for base b b
+        z = np.base_repr(z_int, b, k_n - k_z)
+        # n: z becomes new minion ID
+        n = z
+    return len(list_of_id[list_of_id.index(n):])
 
-    # x: n descending, y: n ascending
-    y = ''.join(sorted(n))
-    x = y[::-1]
-    # z: x - y with preceding zeros to keep length k
-    z = str(int(x) - int(y))
-    z = (k - len(x)) * '0' + z
-    n = z
-    print(n)
-    return(z, k)
 
 if __name__ == "__main__":
     print(solution('1211', 10)) #1
