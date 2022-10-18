43985adb6219fb7ecdf39aa96fe97474cf510174
Justus Evenschor <j.evenschor@gmail.com>
2022-10-18 13:24:08 +0200

    solved challenge 1

--- 01-i-love-lance-janice/solution.py
+++ 01-i-love-lance-janice/solution.py
@@ -1,2 +1,13 @@
-def​ ​solution(x):
-​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
+import string
+
+def solution(x):
+    d = {string.ascii_lowercase[i]: string.ascii_lowercase[::-1][i] for i in range(len(string.ascii_lowercase))}
+    output = ""
+    for letter in x:
+        if letter in d.keys():
+            letter = letter.replace(letter, d[letter])
+        output += letter
+    return output
+
+if __name__ == "__main__":
+    print(solution("Tsrh xszoovmtv rh hloevw!"))
