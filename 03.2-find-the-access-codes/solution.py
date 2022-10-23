e367ad2d6892331447fa6bde8769df2e19ee0465
Justus Evenschor <j.evenschor@gmail.com>
2022-10-23 17:27:49 +0200

    03.2 solved

--- 03.2-find-the-access-codes/solution.py
+++ 03.2-find-the-access-codes/solution.py
@@ -1,29 +1,47 @@
-def solution(n):
-    n = int(n)
+def solution(l):
+    l = sorted(l)
+    n = len(l)
+    divisor_couples = [0] * n
 
-    res = 0
-    while n != 1:
-        if n % 2 == 0:
-            n /= 2
-        elif (n == 3) or (n % 4 == 1):
-            n -= 1
-        elif n % 4 == 3:
-            n += 1
-        res += 1
+    for i in range(1, n-1):
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                divisor_couples[i] += 1
 
-    return res
+    cnt = 0
+    for i in range(2, n):
+        for j in range(1, i):
+            if l[i] % l[j] == 0:
+                cnt += divisor_couples[j]
+
+    return cnt
+
+def solution(l):
+    l = sorted(l)
+    n = len(l)
+    div_pairs = [0] * n
+    cnt = 0
+    for i in range(0, n):
+        #j = 0
+        for j in range(0, i):
+            if l[i] % l[j] == 0:
+                div_pairs[i] += 1
+                cnt += div_pairs[j]
+    return cnt
+
+
+
+
 
 if __name__ == "__main__":
-    print(solution('4')) #2
-
-    print(solution('15')) #5
-
-    print(solution("15"))
-    print(solution("13"))
-
-    print(solution("4") == 2)
-    print(solution("15") == 5)
-    print(solution("13") == 5)
-    print(solution("9") == 4)
-    print(solution("2") == 1)
-    print(solution("1") == 0)
+    print(solution([1, 2, 3, 4, 5, 6])) #3 [[1, 2, 4], [1, 2, 6], [1, 3, 6]]
+    print(solution([1, 1, 1])) #1
+    print(solution([6, 2, 3, 4, 5, 1])) #3 [[1, 2, 4], [1, 2, 6], [1, 3, 6]]
+    '''
+    print (answer("4") == 2)
+    print (answer("15") == 5)
+    print (answer("13") == 5)
+    print (answer("9") == 4)
+    print (answer("2") == 1)
+    print (answer("1") == 0)
+    '''
