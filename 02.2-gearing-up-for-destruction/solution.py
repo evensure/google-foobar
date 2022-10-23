8472b3fef8b4c7fdc6446b5ae4a20c29f5d3d53a
Justus Evenschor <j.evenschor@gmail.com>
2022-10-23 11:12:13 +0200

    02.2 solved

--- 02.2-gearing-up-for-destruction/solution.py
+++ 02.2-gearing-up-for-destruction/solution.py
@@ -1,8 +1,37 @@
+import numpy as np
+from fractions import Fraction
+
+def all_gears_sizes_at_least_one(pegs, r):
+    for i in range(0, len(pegs) -1):
+        if r < 0 or pegs[i+1] - pegs[i] - r < 1:
+            return False
+        r = pegs[i+1] - pegs[i] - r
+    return True
+
 def solution(pegs):
-    num_gears = len(pegs)
-    # impossible if one or less pegs
-    if ((num_gears == 1) or not pegs):
-        return [-1, -1]
+    impossible = [-1, -1]
+    num_pegs = len(pegs)
+    # peg-coefficients for calculation of r0
+    coef = np.ones(num_pegs)
+    coef[1:-1] += 1
+    coef[::2] *= -1
+    coef *= pegs
+    coef = np.array([Fraction(coef[i]) for i in range(num_pegs)])
+
+    r0 = np.sum(coef)
+    if num_pegs % 2 == 0:
+        even = True
+        multiplier = Fraction(2, 3)
+    else:
+        even = False
+        multiplier = Fraction(2)
+    r0 *= multiplier
+
+    if r0 < 1:
+        return impossible
+    elif all_gears_sizes_at_least_one(pegs, r0):
+        return [r0.numerator, r0.denominator]
+    return impossible
 
 
 
